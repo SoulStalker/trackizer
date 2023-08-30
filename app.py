77a82326326d2c18e915d7afddcf5ac029dc16d4
Almaz Galimov <almazpython@gmail.com>
2023-08-30 15:44:06 +0500

    [upd] Start again. CRUD works in simple dict

--- app.py
+++ app.py
@@ -1,12 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, request
 
-apple = Flask(__name__)
+app = Flask(__name__)
+
+tasks = {
+            "1": {'title': 'step1', 'description': 'do crud in simple list'},
+            "2": {'title': 'step2', 'description': 'another info'}
+        }
 
 
-@apple.route('/')
-def index():
-    return 'Life of coder'
+@app.route('/api/tasks', methods=['GET'])
+def get_list():
+    return jsonify(tasks)
+
+
+@app.route('/api/tasks', methods=['POST'])
+def update_list():
+    new_task = request.json
+    tasks.update(new_task)
+    return jsonify(tasks)
+
+
+@app.route('/api/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    if not str(task_id) in tasks.keys():
+        return {'message': 'No task with this ID'}, 400
+    task = tasks[str(task_id)]
+    params = request.json
+    task.update(params)
+    return task
+
+
+@app.route('/api/tasks/<int:task_id>', methods=['DELETE'])
+def del_task(task_id):
+    if not str(task_id) in tasks.keys():
+        return {'message': 'No task with this ID'}, 400
+    tasks.pop(str(task_id))
+    return jsonify(tasks)
 
 
 if __name__ == '__main__':
-    apple.run(debug=True)
+    app.run(debug=True)
--- controllers.py
+++ controllers.py
@@ -1,56 +1,9 @@
-from flask import request
-from flask_restx import Namespace, Resource
-from models import Task, TaskState, Base
-from config import DSN
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from models import Task, TaskState, Base, engine
 
 
-engine = create_engine(DSN)
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-
-api = Namespace('tasks', description='Task related operations')
-
-
-@api.route('/daily')
-class DailyTask(Resource):
-    def get(self):
-        daily_task = Task.query.filter(Task.state.has(TaskState.name == 'daily')).all()
-        return [task.to_dict() for task in daily_task], 200
-
-    def post(self):
-        data = request.json
-        new_task = Task(title=data['title'], description=data['description'])
-        session.add(new_task)
-        session.commit()
-        return new_task.to_dict(), 201
-
-
-@api.route('/current')
-class CurrentTask(Resource):
-    def get(self):
-        current_tasks = Task.query.filter(Task.state.has(TaskState.name == 'current')).all()
-        return [task.to_dict() for task in current_tasks], 200
-
-    def post(self):
-        data = request.json
-        new_task = Task(title=data['title'], description=data['description'], state_id=2)
-        # todo soulstalker поправить current_state_id после миграций
-        session.add(new_task)
-        session.commit()
-        return new_task.to_dict(), 201
-
-
-@api.route('/completed')
-class CompletedTask(Resource):
-    def get(self):
-        competed_tasks = Task.query.filter(Task.state.has(TaskState.name == 'completed')).all
-        return [task.to_dict() for task in competed_tasks], 200
-
-
-api.add_resource(DailyTask, '/daily')
-api.add_resource(CurrentTask, '/current')
-api.add_resource(CompletedTask, '/completed')
