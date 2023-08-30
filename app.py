eb6096d293ecc9d81faf1a4c50955bd552d00b7b
Almaz Galimov <almazpython@gmail.com>
2023-08-30 18:36:33 +0500

    [upd] Begin working with SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, jsonify, request
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from config import DSN
 
 app = Flask(__name__)
 
+engine = create_engine(DSN)
+session = scoped_session(sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+))
+
+Base = declarative_base()
+Base.query = session.query_property()
+
+from models import *
+
+Base.metadata.create_all(bind=engine)
+
 tasks = {
-            "1": {'title': 'step1', 'description': 'do crud in simple list'},
-            "2": {'title': 'step2', 'description': 'another info'}
-        }
+    "1": {'title': 'step1', 'description': 'do crud in simple list'},
+    "2": {'title': 'step2', 'description': 'do it in sqlalchemy'}
+}
 
 
 @app.route('/api/tasks', methods=['GET'])
@@ -36,6 +53,11 @@ def del_task(task_id):
         return {'message': 'No task with this ID'}, 400
     tasks.pop(str(task_id))
     return jsonify(tasks)
+
+
+@app.teardown_appcontext
+def close_session(exeption=None):
+    session.remove()
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,8 +1,9 @@
+from app import db, session, Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Time, create_engine
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from config import DSN
-Base = declarative_base()
+# from config import DSN
+# Base = declarative_base()
 
 
 class TaskState(Base):
@@ -10,7 +11,7 @@ class TaskState(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(32), nullable=False)
-    description = Column(String(200), nullable=True)
+    description = Column(String(500), nullable=True)
 
 
 class Task(Base):
@@ -18,7 +19,7 @@ class Task(Base):
 
     id = Column(Integer, primary_key=True)
     title = Column(String(100), nullable=False)
-    description = Column(String(200), nullable=True)
+    description = Column(String(500), nullable=True)
     start_time = Column(Time, nullable=True)
     end_time = Column(Time, nullable=True)
     completed = Column(Boolean, default=False)
@@ -38,5 +39,5 @@ class Task(Base):
         }
 
 
-engine = create_engine(DSN)
-Base.metadata.create_all(engine)
+# engine = create_engine(DSN)
+# Base.metadata.create_all(engine)
